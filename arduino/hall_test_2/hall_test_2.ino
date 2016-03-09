c2aa8d95b574d91fb8d73994dcc44cab43c113d2
Pat <barton.pj@gmail.com>
2016-03-09 11:22:41 -0800

    rearranged test images

---
+++ arduino/hall_test_2/hall_test_2.ino
@@ -0,0 +1,28 @@
+/*
+ *sensor to analog0; 5v feed (red); ground (black)
+ */
+
+int last = 1000;
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.print("Loaded setup");
+}
+
+void DoMeasurement()
+{
+// measure magnetic field
+  int raw = analogRead(0);   // Range : 0..1024
+  Serial.println(raw);
+
+}
+
+void loop() 
+{
+    int raw = analogRead(0);
+  // if the sensor reading is greater than the threshold:
+  if (raw <= 2) {
+     Serial.println("Door open!  Hall output is: " + String(raw) + "Spencer is a rat!");
+  }
+    delay(100);
+}
---
+++ arduino/hall_test_3/hall_test_3.ino
@@ -0,0 +1,48 @@
+/*
+ *sensor to analog0; 5v feed (red); ground (black)
+ */
+ 
+//from example provided with Rand A
+#include "RAComm.h"       // Use Raspberry command library
+
+#define CMD1 "GetRTC -s"  // get timestamp
+
+#define LOGFILE  "/home/pi/wakeup.dat"  // where to write message
+
+#define CHECKTIME 10000    // check interval 10 sec
+
+RAComm Cmd;               // Cmd is the RAComm class
+
+int status=0;          // status flag (when condition true, set flag 1)
+                       // it doesn't restart procedure until status will be back to 0
+boolean flagOn;        // flag Raspberry on or off
+
+int sensorPin = A1;    // select the input pin for the potentiometer or photores
+int ledPin = 13;       // select the pin for the LED
+int sensorValue = 0;   // variable to store the value coming from the sensor
+int threshold =2;    // threshold of sensorPin 
+int switchPin = 4;     // pin for switching 
+
+long ta=0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);     // led just as flag
+  pinMode(switchPin, OUTPUT);  
+  digitalWrite(ledPin,0);      // led off
+  digitalWrite(switchPin,0);   // initialize switching pin
+
+  Serial.begin(9600);
+  Serial.print("Loaded setup");
+}
+
+void loop() 
+{
+    int raw = analogRead(sensorPin);
+  // if the sensor reading is greater than the threshold:
+  if (raw <= threshold) {
+     Serial.println("Door open!  Hall output is: " + String(raw) + " Spencer is a rat!");
+     digitalWrite(switchPin,HIGH);
+     Serial.println("Switching pi on ...");
+  }
+    delay(100);
+}
